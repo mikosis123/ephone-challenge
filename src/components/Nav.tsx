56bc1fda8d5abb97c5b1e11f7ec51ce7f41e6d3f
mikosis123 <mikosis93@gmail.com>
2024-04-20 06:32:39 +0300

    color adjustment

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,10 +5,10 @@ const Nav = () => {
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-gray-200 p-6">
         <div className="flex items-center  flex-shrink-0 text-white mr-6 w-full">
-          <h1 className="font-semibold shadow-sm  text-2xl text-black tracking-tight">
+          <h1 className="font-semibold shadow-sm  text-2xl text-[#3730a3] tracking-tight">
             ReactFlow Menu Extraction and Visualization Challenge
           </h1>
-          <span className="ml-3 text-xl ml-auto text-black">
+          <span className="ml-3 text-xl ml-auto text-[#3730a3]">
             EPphone Challange
           </span>
         </div>
